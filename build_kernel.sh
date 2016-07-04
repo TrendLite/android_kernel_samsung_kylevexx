c5b4acd858d2df93d74da28edce0447e50f57143
grace5921 <androidlover5842@gmail.com>
2016-07-04 22:02:52 +0530

    add make zip and boot option

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,7 +4,79 @@ red=`tput setaf 1`
 blue=`tput setaf 4`
 default=`tput sgr0`
 PS3='Please enter your choice: '
-options=("$blue KYLEVE $default" "$green KYLEVESS $default" "$red CLEAN $default" )
+function boot
+{
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Boot.img"
+TITLE="COMPILE BOOT.IMG FOR ? :-"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "KYLEVE"
+         2 "KYLEVESS"
+         3 "QUIT")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "You chose Option 1"
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+
+esac
+}
+
+function make_zip
+{
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="ZIP"
+TITLE="MAKE FLASHABLE ZIP FOR ? :-"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "KYLEVE"
+         2 "KYLEVESS"
+         3 "QUIT")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "You chose Option 1"
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+
+esac
+}
+
+options=("$blue KYLEVE $default" "$green KYLEVESS $default" "$red CLEAN $default" "MAKE BOOT" "MAKE FLASHABLE ZIP" )
 select opt in "${options[@]}"
 do
     case $opt in
@@ -27,10 +99,22 @@ tput setaf 1
 make -j4 mrproper
 echo -e "$blue Made clean $default"
 tput sgr0
+clear
+clear
+exit
+;;
+        "MAKE BOOT")
+boot
+exit
+;;
+        "MAKE FLASHABLE ZIP")
+make_zip
 exit
             break
             ;;
         *) echo invalid option;;
     esac
 done
+
+
 
